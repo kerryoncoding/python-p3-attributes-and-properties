7a7c00a93d2dd543432fa66903b532d64ac72915
Kerry Guarino <kerry.guarino@gmail.com>
2023-09-09 13:27:02 -0400

    passing dog

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,22 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+    def __init__(self, name = "fido"):
+        self.name = name
+    
+    def get_name(self):
+        print("getting name")
+        return self._name
+
+    def set_name(self, name):
+        # if (isinstance(name, str)) and (1 < len(name) < 25):
+        if (type(name) == str) and (1 < len(name) < 25):
+            print(f"Setting name to: {name}.")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+
